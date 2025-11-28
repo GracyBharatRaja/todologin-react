1b8f7a17bfeef18db27bb051e192b882037199be
bharatgracy <gracybharat100@gmail.com>
2025-11-28 09:06:27 +0530

    after deloyment initial change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: ['babel-plugin-react-compiler',tailwindcss(),react()],
+  base: '/todologin-react/',
+  plugins: [tailwindcss(),react()],
 })
